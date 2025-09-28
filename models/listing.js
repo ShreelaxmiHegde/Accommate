ce1137f2b68badc945812cf2e0e4b661006d481a
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-09-28 17:24:41 +0530

    Corrected type mistakes

--- models/listing.js
+++ models/listing.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const listningSchema = new Schema({
+const listingSchema = new Schema({
     title: {
         type: String,
         required: true
@@ -35,5 +35,5 @@ const listningSchema = new Schema({
     },
 });
 
-const Listning = mongoose.model("Listing", listningSchema);
-module.exports = Listning;
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
