79043ea99c0e7fdec430c563cde95390d52a37d5
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-10-09 19:48:03 +0530

    connected listing schema with reviews schema

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { Review } = require("./reviews");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -33,6 +34,12 @@ const listingSchema = new Schema({
          "https://images.unsplash.com/vector-1756301725630-0dba7e612f04?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGhvc3RlbCUyMHJvb218ZW58MHx8MHx8fDA%3D"
         : v //image link is empty
     },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
