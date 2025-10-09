5788bed58e76bb9560066dbd83e198d13bbd5dcc
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-10-09 19:51:49 +0530

    Add review schema for Joi validation

--- schema.js
+++ schema.js
@@ -2,7 +2,7 @@
 
 const Joi = require("joi");
 
-const listingSchema = Joi.object({
+module.exports.listingSchema = Joi.object({
     listing : Joi.object({
         title : Joi.string().required().min(5).max(50),
         desc : Joi.string().required().min(10),
@@ -14,4 +14,9 @@ const listingSchema = Joi.object({
     }).required()
 });
 
-module.exports = { listingSchema };
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number.required().min(1).max(5),
+        Comment: Joi.string.required().min(3)
+    }).required()
+});
