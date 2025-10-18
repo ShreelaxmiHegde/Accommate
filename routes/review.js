809ff1f6e1e740a307cdebda042d00091e2db147
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-10-18 18:27:23 +0530

    Add Review authorization

--- routes/review.js
+++ routes/review.js
@@ -3,12 +3,13 @@ const router = express.Router({ mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync");
 const Listing = require("../models/listing.js");
 const Review = require("../models/reviews.js");
-const { validateReview } = require("../middleware.js");
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js");
 
 // reviews post route
-router.post("/", validateReview, wrapAsync(async (req, res) => {
+router.post("/", isLoggedIn, validateReview, wrapAsync(async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
+    newReview.author = req.user._id;
 
     listing.reviews.push(newReview);
 
@@ -21,7 +22,7 @@ router.post("/", validateReview, wrapAsync(async (req, res) => {
 }));
 
 // delete review route 
-router.delete("/:reviewId", wrapAsync(async(req, res) => {
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, wrapAsync(async(req, res) => {
     let { id, reviewId } = req.params;
 
     await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}}); //pull operator to remove the reference of review from listing
