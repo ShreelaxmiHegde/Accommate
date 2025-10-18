bff98ff891153469330832be8c6dfde33dc5a552
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-10-18 10:44:31 +0530

    Add owner authorization middleware(isOwner)
    Include validateListing and validateReview middlewares

--- middleware.js
+++ middleware.js
@@ -1,3 +1,8 @@
+const Listing = require("./models/listing");
+const { listingSchema } = require("./schema.js"); //schema validation
+const ExpressError = require("./utils/ExpressError.js");
+const { reviewSchema } = require("./schema.js"); //schema validation
+
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) {
         // redirect url after login
@@ -14,4 +19,37 @@ module.exports.saveRedirectUrl = (req, res, next) => {
         res.locals.redirectUrl = req.session.redirectUrl;
     }
     next();
+}
+
+module.exports.isOwner = async (req, res, next) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    if(!listing.owner.equals(res.locals.currUser._id)) {
+        req.flash("error", "Access Denied!");
+        return res.redirect(`/listings/${id}`);
+    }
+
+    next();
+}
+
+// form data validation error handling middleware
+module.exports.validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        next(new ExpressError(400, errMsg)); //call next err handler middleware
+    } else {
+        next();
+    }
+}
+
+// review validation
+module.exports.validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        next(new ExpressError(400, errMsg)); //call next err handler middleware
+    } else {
+        next();
+    }
 }
