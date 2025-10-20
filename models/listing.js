c47aee70872e8f23b04ceb9768d164fe35d9e5a5
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-10-20 18:03:46 +0530

    Update model with image option

--- models/listing.js
+++ models/listing.js
@@ -28,11 +28,8 @@ const listingSchema = new Schema({
         required: true
     },
     image: {
-        type: String,
-        default: "https://images.unsplash.com/vector-1756301725630-0dba7e612f04?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGhvc3RlbCUyMHJvb218ZW58MHx8MHx8fDA%3D", //show if image not added
-        set: (v) => v === "" ? 
-         "https://images.unsplash.com/vector-1756301725630-0dba7e612f04?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGhvc3RlbCUyMHJvb218ZW58MHx8MHx8fDA%3D"
-        : v //image link is empty
+        url: String,
+        filename: String
     },
     reviews: [
         {
