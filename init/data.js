1b92fe5273369acfe1a5ad9172d03ae9f235ad47
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-10-20 18:01:36 +0530

    Reinitialize data with image (url)

--- init/data.js
+++ init/data.js
@@ -6,7 +6,10 @@ const sampleListings = [
     "state": "Delhi",
     "price": 7500,
     "capacity": 2,
-    "image": "https://images.unsplash.com/photo-1709805619372-40de3f158e83?q=80&w=2095&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    "image": {
+      filename: "ListingImage",
+      url: "https://images.unsplash.com/photo-1709805619372-40de3f158e83?q=80&w=2095&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
   },
   {
     "title": "Shared Apartment in Pune",
@@ -15,7 +18,10 @@ const sampleListings = [
     "state": "Maharashtra",
     "price": 9500,
     "capacity": 3,
-    "image": "https://images.unsplash.com/photo-1505691723518-36a5ac3be353"
+    "image": {
+      filename: "ListingImage",
+      url: "https://images.unsplash.com/photo-1505691723518-36a5ac3be353"
+    }
   },
   {
     "title": "Girls Hostel near IIT Bombay",
@@ -24,7 +30,10 @@ const sampleListings = [
     "state": "Maharashtra",
     "price": 12000,
     "capacity": 1,
-    "image": "https://images.unsplash.com/photo-1600585154340-be6161a56a0c"
+    "image": {
+      filename: "ListingImage",
+      url: "https://images.unsplash.com/photo-1600585154340-be6161a56a0c"
+    }
   },
   {
     "title": "Budget PG near Bangalore University",
@@ -33,7 +42,10 @@ const sampleListings = [
     "state": "Karnataka",
     "price": 6000,
     "capacity": 3,
-    "image": "https://images.unsplash.com/photo-1600585154526-990dced4db0d"
+    "image": {
+      filename: "ListingImage",
+      url: "https://images.unsplash.com/photo-1600585154526-990dced4db0d"
+    }
   },
   {
     "title": "Luxury Student Flats in Hyderabad",
@@ -42,7 +54,10 @@ const sampleListings = [
     "state": "Telangana",
     "price": 15000,
     "capacity": 2,
-    "image": "https://images.unsplash.com/photo-1501183638710-841dd1904471"
+    "image": {
+      filename: "ListingImage",
+      url: "https://images.unsplash.com/photo-1501183638710-841dd1904471"
+    }
   },
   {
     "title": "PG near Jadavpur University",
@@ -51,7 +66,10 @@ const sampleListings = [
     "state": "West Bengal",
     "price": 5500,
     "capacity": 2,
-    "image": "https://images.unsplash.com/photo-1502005097973-6a7082348e28"
+    "image": {
+      filename: "ListingImage",
+      url: "https://images.unsplash.com/photo-1502005097973-6a7082348e28"
+    }
   },
   {
     "title": "Student Room near Manipal University",
@@ -60,7 +78,10 @@ const sampleListings = [
     "state": "Karnataka",
     "price": 8500,
     "capacity": 1,
-    "image": "https://images.unsplash.com/photo-1594511936753-5cd40f0967ba?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fHw%3D"
+    "image": {
+      filename: "ListingImage",
+      url: "https://images.unsplash.com/photo-1594511936753-5cd40f0967ba?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fHw%3D"
+    } 
   },
   {
     "title": "PG near Chandigarh University",
@@ -69,7 +90,10 @@ const sampleListings = [
     "state": "Punjab",
     "price": 7000,
     "capacity": 2,
-    "image": "https://images.unsplash.com/photo-1576095910326-9de5a8b207e4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D"
+    "image": {
+      filename: "ListingImage",
+      url: "https://images.unsplash.com/photo-1576095910326-9de5a8b207e4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D"
+    }
   },
   {
     "title": "Affordable Hostel near BHU",
@@ -78,7 +102,10 @@ const sampleListings = [
     "state": "Uttar Pradesh",
     "price": 4500,
     "capacity": 3,
-    "image": "https://images.unsplash.com/photo-1555854877-bab0e564b8d5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG9zdGVsJTIwcm9vbXxlbnwwfHwwfHx8MA%3D%3D"
+    "image": {
+      filename: "ListingImage",
+      url: "https://images.unsplash.com/photo-1555854877-bab0e564b8d5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG9zdGVsJTIwcm9vbXxlbnwwfHwwfHx8MA%3D%3D"
+    } 
   },
   {
     "title": "Studio Room near SRM University",
@@ -87,7 +114,10 @@ const sampleListings = [
     "state": "Tamil Nadu",
     "price": 9500,
     "capacity": 1,
-    "image": "https://images.unsplash.com/photo-1555854877-bab0e564b8d5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG9zdGVsJTIwcm9vbXxlbnwwfHwwfHx8MA%3D%3D"
+    "image": {
+      filename: "ListingImage",
+      url: "https://images.unsplash.com/photo-1555854877-bab0e564b8d5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG9zdGVsJTIwcm9vbXxlbnwwfHwwfHx8MA%3D%3D"
+    }
   },
   {
     "title": "Shared Flat near Christ University",
@@ -96,7 +126,10 @@ const sampleListings = [
     "state": "Karnataka",
     "price": 10000,
     "capacity": 2,
-    "image": "https://plus.unsplash.com/premium_photo-1717014210742-b3f268ff3b6e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D"
+    "image": {
+      filename: "ListingImage",
+      url: "https://plus.unsplash.com/premium_photo-1717014210742-b3f268ff3b6e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D"
+    }
   },
   {
     "title": "Boys Hostel near AMU",
@@ -105,7 +138,10 @@ const sampleListings = [
     "state": "Uttar Pradesh",
     "price": 5000,
     "capacity": 4,
-    "image": "https://images.unsplash.com/photo-1651902387099-787f4a62a3e3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDExfHx8ZW58MHx8fHx8"
+    "image": {
+      filename: "ListingImage",
+      url: "https://images.unsplash.com/photo-1651902387099-787f4a62a3e3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDExfHx8ZW58MHx8fHx8"
+    }
   },
   {
     "title": "Modern PG near Symbiosis",
@@ -114,7 +150,10 @@ const sampleListings = [
     "state": "Maharashtra",
     "price": 11000,
     "capacity": 2,
-    "image": "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267"
+    "image": {
+      filename: "ListingImage",
+      url: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267"
+    }
   },
   {
     "title": "Girls PG near DU South Campus",
@@ -123,7 +162,10 @@ const sampleListings = [
     "state": "Delhi",
     "price": 8000,
     "capacity": 2,
-    "image": "https://images.unsplash.com/photo-1694559218346-25ac5707cfa3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8"
+    "image": {
+      filename: "ListingImage",
+      url: "https://images.unsplash.com/photo-1694559218346-25ac5707cfa3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8"
+    }
   },
   {
     "title": "Shared Accommodation near LPU",
@@ -132,7 +174,10 @@ const sampleListings = [
     "state": "Punjab",
     "price": 6500,
     "capacity": 3,
-    "image": "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688"
+    "image": {
+      filename: "ListingImage",
+      url: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688"
+    }
   },
   {
     "title": "Hostel near NIT Trichy",
@@ -141,7 +186,10 @@ const sampleListings = [
     "state": "Tamil Nadu",
     "price": 5500,
     "capacity": 3,
-    "image": "https://plus.unsplash.com/premium_photo-1683649964203-baf13fa852e4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aG9zdGVsJTIwcm9vbXxlbnwwfHwwfHx8MA%3D%3D"
+    "image": {
+      filename: "ListingImage",
+      url: "https://plus.unsplash.com/premium_photo-1683649964203-baf13fa852e4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aG9zdGVsJTIwcm9vbXxlbnwwfHwwfHx8MA%3D%3D"
+    }
   },
   {
     "title": "Private Room near IISc Bangalore",
@@ -150,7 +198,10 @@ const sampleListings = [
     "state": "Karnataka",
     "price": 12000,
     "capacity": 1,
-    "image": "https://images.unsplash.com/photo-1623625434462-e5e42318ae49?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG9zdGVsJTIwcm9vbXxlbnwwfHwwfHx8MA%3D%3D"
+    "image": {
+      filename: "ListingImage",
+      url: "https://images.unsplash.com/photo-1623625434462-e5e42318ae49?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG9zdGVsJTIwcm9vbXxlbnwwfHwwfHx8MA%3D%3D"
+    }
   },
   {
     "title": "Shared PG near Osmania University",
@@ -159,7 +210,10 @@ const sampleListings = [
     "state": "Telangana",
     "price": 7000,
     "capacity": 2,
-    "image": "https://plus.unsplash.com/premium_photo-1682093002327-087f25034765?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGhvc3RlbCUyMHJvb218ZW58MHx8MHx8fDA%3D"
+    "image": {
+      filename: "ListingImage",
+      url: "https://plus.unsplash.com/premium_photo-1682093002327-087f25034765?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGhvc3RlbCUyMHJvb218ZW58MHx8MHx8fDA%3D"
+    }
   },
   {
     "title": "Hostel near Patna University",
@@ -168,7 +222,10 @@ const sampleListings = [
     "state": "Bihar",
     "price": 4800,
     "capacity": 3,
-    "image": "https://images.unsplash.com/photo-1501183638710-841dd1904471"
+    "image": {
+      filename: "ListingImage",
+      url: "https://images.unsplash.com/photo-1501183638710-841dd1904471"
+    }
   },
   {
     "title": "Girls Hostel near Gujarat University",
@@ -177,7 +234,10 @@ const sampleListings = [
     "state": "Gujarat",
     "price": 9000,
     "capacity": 2,
-    "image": "https://images.unsplash.com/photo-1552858725-693709cc17c7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGhvc3RlbCUyMHJvb218ZW58MHx8MHx8fDA%3D"
+    "image": {
+      filename: "ListingImage",
+      url: "https://images.unsplash.com/photo-1552858725-693709cc17c7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGhvc3RlbCUyMHJvb218ZW58MHx8MHx8fDA%3D"
+    }
   }
 ];
 
