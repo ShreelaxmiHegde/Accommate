9be628261d76e2d26d2eb9357371cebde7e3a96d
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-11-16 15:33:50 +0530

    update navbar and footer stylings
    connect auth btns in navbar with auth components

---
+++ client/src/components/layouts/Navbar.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Tabs,
+  Tab,
+  Button,
+  IconButton,
+  Typography,
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemButton,
+} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import TravelExploreIcon from "@mui/icons-material/TravelExplore";
+import AddBusinessIcon from "@mui/icons-material/AddBusiness";
+import MenuIcon from "@mui/icons-material/Menu";
+import AuthDialog from "../auth/AuthDialog";
+
+export default function Navbar() {
+  const navItems = ["Home", "Find Stays", "Become a Host"];
+  const [initialMode, setInitialMode] = useState("");
+  const [authOpen, setAuthOpen] = useState(false);
+
+  const handleModeChange = (mode) => {
+    setInitialMode(mode);
+    setAuthOpen(true);
+  }
+
+  const fontDefStyle = {
+    textTransform: "none", 
+    fontWeight: 500
+  };
+
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  return (
+    <AppBar position="sticky" color="transparent" elevation={3} sx={{backgroundColor:"white"}}>
+      <Toolbar sx={{ display: "flex", justifyContent:{xs:"space-between", sm:"space-around"} , alignItems: "center", color: "#2563eb", fontWeight: 700 }}>
+        <Typography variant="h5" sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+          <i className="fa-solid fa-house-tsunami" style={{ color: "#2563eb", fontSize: "1.7rem" }}></i> <span>Accommate</span>
+        </Typography>
+
+        <Tabs centered sx={{display: {xs:"none", sm:"flex"}, gap:2}}>
+          <Tab icon={<HomeIcon />} sx={fontDefStyle}  label="Home" />
+          <Tab icon={<TravelExploreIcon />} label="Find Stays" sx={fontDefStyle}  />
+          <Tab icon={<AddBusinessIcon />} label="Become a Host" sx={fontDefStyle}  />
+        </Tabs>                  
+
+        <Box direction="row" spacing={2} sx={{display: {xs:"none", sm:"flex"}, gap:2}}>
+          <Button variant="outlined" sx={fontDefStyle} onClick={() => handleModeChange("login")}>Log In</Button>
+          <Button variant="contained" sx={fontDefStyle} onClick={() => handleModeChange("signup")}>Sign Up</Button>
+        </Box>
+
+        <IconButton sx={{ display: { xs: "flex", sm: "none" } }} onClick={handleDrawerToggle}>
+          <MenuIcon />
+        </IconButton>
+
+        <Drawer anchor="right" open={mobileOpen} onClose={handleDrawerToggle}
+          sx={{
+            "& .MuiDrawer-paper": { width: 200, height: 250, paddingTop: 2 },
+          }}>
+
+          <List>
+            {navItems.map((item) => (
+              <ListItem key={item} disablePadding>
+                <ListItemButton onClick={handleDrawerToggle}>
+                  <ListItemText primary={item} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+            <Box sx={{display: "flex", flexDirection: "column", gap: 1, alignItems: "start", paddingLeft: 2}}>
+              <Button variant="outlined" sx={fontDefStyle} onClick={() => handleModeChange("login")}>Log In</Button>
+              <Button variant="contained" sx={fontDefStyle} onClick={() => handleModeChange("signup")}>Sign Up</Button>
+            </Box>
+          </List>
+        </Drawer>
+
+        <AuthDialog open={authOpen} initialMode={initialMode} onClose={() => setAuthOpen(false)} /> 
+      </Toolbar>
+    </AppBar>
+  );
+};
