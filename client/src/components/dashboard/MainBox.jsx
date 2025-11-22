6cfd55c6041680b36c24c9cddfc56064633f5137
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-11-22 16:33:22 +0530

    separated main section as another component

---
+++ client/src/components/dashboard/MainBox.jsx
@@ -0,0 +1,40 @@
+import {
+    Button,
+    Typography,
+    Box
+} from "@mui/material";
+import "./SearchBar"
+import SearchBar from "./SearchBar";
+
+export default function MainBox() {
+    return (
+        <section>
+            <Box
+                sx={{
+                    width: "100%",
+                    backgroundColor: "#215da9",
+                    textAlign: "center",
+                    color: 'white',
+                    paddingTop: 5
+                }}>
+                <Typography variant="h3"
+                    sx={{
+                        fontWeight: 800,
+                        letterSpacing: "-1px",
+                    }}
+                >Live closer, hustle harder
+                </Typography>
+                <Typography variant="h6" sx={{ fontWeight: 300 }}>
+                    Find your Student Stay. Effortlessly.
+                </Typography>
+
+                <Box sx={{marginTop: "2rem", display:"flex", flexWrap: "wrap", alignItems: "center", justifyContent:"center"}}>
+                    <Button variant="contained" sx={{backgroundColor:"#f6cb3cff", color:"black", marginInline:"0.5rem"}}>Explore Stays</Button>
+                    <Button variant="contained"sx={{marginInline:"0.5rem"}}>Post a Stay</Button>
+                </Box>
+
+                <SearchBar />
+            </Box>
+        </section>
+    )
+}
