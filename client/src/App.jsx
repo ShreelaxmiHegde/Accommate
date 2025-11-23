07c0dc86d9bbed65eaa7d74c12a6cb0fd8c7ab23
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-11-23 17:35:41 +0530

    page routings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,26 @@
+import { Routes, Route } from "react-router-dom"
+
 import Navbar from "./components/layouts/Navbar"
-import './index.css'
 import Footer from "./components/layouts/Footer"
-import ListingPage from "./components/listingPage/ListingPage.jsx"
-import Hero from "./components/dashboard/Hero.jsx"
+
+import Hero from "./pages/Hero"
+import ListingExplore from "./pages/ListingExplore"
+import ListingPage from "./pages/ListingPage"
+import NotFound from "./pages/NotFound"
+import './index.css'
 
 function App() {
 
   return (
     <>
       <Navbar />
-      <Hero />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/explore" element={<ListingExplore />} />
+        <Route path="/listing" element={<ListingPage />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
     </>
   )
