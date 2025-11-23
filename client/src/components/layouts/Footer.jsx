0441b1707e69b3b227438fb56025f5203ce4248c
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-11-23 09:41:19 +0530

    add footer margin top

--- client/src/components/layouts/Footer.jsx
+++ client/src/components/layouts/Footer.jsx
@@ -2,7 +2,17 @@ import { Box, Typography, Link } from "@mui/material";
 
 export default function Footer() {
     return (
-        <Box component="footer" sx={{backgroundColor:"#f5f5f5", padding:2, display:"flex", flexWrap:"wrap", justifyContent:"space-around", alignItems:"center", gap:2}}>
+        <Box component="footer" 
+            sx={{
+                backgroundColor:"#f5f5f5", 
+                padding:2, 
+                display:"flex", 
+                flexWrap:"wrap", 
+                justifyContent:"space-around", 
+                alignItems:"center", 
+                gap:2,
+                mt: 10
+            }}>
             <Typography sx={{textAlign:"center"}}>Copyright &copy; 2025 Accommate. All rights reserved.</Typography>
             <Box sx={{display:"flex", gap:2, flexWrap:"wrap"}}>
                 <Link href="/support#about" underline="hover" color="text.secondary" varient="body2">About</Link>
