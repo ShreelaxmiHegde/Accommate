e4b10cdcd4daa3f2573a0f1138362820810cb9ba
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-11-25 18:14:12 +0530

    fetch listing data and reflect it in listings explore page

--- client/src/components/listingsExplore/ListingCarousel.jsx
+++ client/src/components/listingsExplore/ListingCarousel.jsx
@@ -1,15 +1,25 @@
 import ListingCard from "./ListingCard"
+import { Box, Grid } from "@mui/material";
 
-export default function ListingCarousel() {
-
-    const carousel = {
-        display: "flex",
-        gap: 1,
-    }
-
+export default function ListingCarousel({ listings }) {
     return (
-        <div className={carousel}>
-            <ListingCard />
-        </div>
+        <Grid container wrap="wrap"
+            sx={{
+                width: "90%",
+                mx: "auto",
+                mt: "3rem",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+            {listings.map((listing) => (
+                <Box sx={{
+                    width: { xs: 250, md: 210 },
+                    marginRight: 2,
+                    my: 5,
+                }}>
+                    <ListingCard listing={listing} />
+                </Box >
+            ))}
+        </Grid>
     )
 }
--- client/src/pages/ListingExplore.jsx
+++ client/src/pages/ListingExplore.jsx
@@ -1,11 +1,41 @@
-
+import { useEffect, useState } from "react"
+import api from "../api/axios"
+import ListingCarousel from "../components/listingsExplore/ListingCarousel"
+import { Box } from "@mui/material"
+import ListingExploreSkeleton from "../components/skeletons/ListingExploreSkeleton"
+import SearchBar from "../components/dashboard/SearchBar"
 
 export default function ListingExplore() {
+    const [listings, setListings] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const fetchListings = async () => {
+        try {
+            setLoading(true); //start loading
+            let res = await api.get("/listings");
+            setListings(res.data);
+        } catch (err) {
+            console.log(err);
+        } finally {
+            setLoading(false); //stop loading
+        }
+    }
+
+    useEffect(() => {
+        fetchListings();
+    }, []);
 
     return (
         <>
-        <h1>Explore listings</h1>
-        <p>Browse your campus-specific housing options</p>
+            <Box sx={{ paddingTop: "5rem" }}>
+                <SearchBar />
+
+                {loading ? 
+                    (<ListingExploreSkeleton />)
+                    : 
+                    (<ListingCarousel listings={listings} />)
+                }
+            </Box>
         </>
     )
 }
