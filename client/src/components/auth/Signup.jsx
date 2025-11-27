ca73bc2be511f821878c17c9bfbec792cdd55a5f
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-11-27 15:40:06 +0530

    Lifting state up: sent entered data to parent component(AuthDialog) through onChange prop

--- client/src/components/auth/Signup.jsx
+++ client/src/components/auth/Signup.jsx
@@ -1,51 +1,77 @@
 import { useState } from "react";
-import { 
-    TextField,
-    Box,
-    FormControl,
-    OutlinedInput,
-    InputAdornment,
-    InputLabel,
-    IconButton
+import {
+  TextField,
+  Box,
+  FormControl,
+  OutlinedInput,
+  InputAdornment,
+  InputLabel,
+  IconButton
 } from "@mui/material";
-import Visibility from '@mui/icons-material/Visibility';
+import {
+  Visibility,
+  VisibilityOff
+} from '@mui/icons-material';
 
-export default function Signup() {
-    const [showPassword, setShowPassword] = useState(false);
+export default function Signup({ onChange }) {
+  const [showPassword, setShowPassword] = useState(false);
 
-    const handleClickShowPassword = () => {
-        setShowPassword((show) => !show)
-    };
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
 
-    const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-    };
-    const handleMouseUpPassword = (event) => {
-        event.preventDefault();
-    };
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
-    return (
-        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-            <TextField id="outlined-basic" label="Username" variant="outlined" fullWidth required />
-            <TextField id="outlined-basic" type="email" label="Email" variant="outlined" fullWidth required />
-            <FormControl sx={{ m: 0 }} variant="outlined" fullWidth required>
-              <InputLabel htmlFor="outlined-adornment-password" fullWidth>Password</InputLabel>
-              <OutlinedInput
-                id="outlined-adornment-password"
-                type={showPassword ? 'text' : 'password'}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label={showPassword ? 'hide the password' : 'display the password'}
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      onMouseUp={handleMouseUpPassword}
-                      edge="end">
-                      {showPassword ? <ilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                } label="Password" />
-            </FormControl>
-        </Box>
-    );
+  const handleMouseUpPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+      <TextField
+        id="outlined-basic"
+        label="Username"
+        variant="outlined"
+        name="username"
+        onChange={onChange}
+        fullWidth
+        required
+      />
+      <TextField
+        id="outlined-basic"
+        type="email"
+        label="Email"
+        variant="outlined"
+        name="email"
+        onChange={onChange}
+        fullWidth
+        required
+      />
+      <FormControl sx={{ m: 0 }} variant="outlined" fullWidth required>
+        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          name="password"
+          onChange={onChange}
+          type={showPassword ? 'text' : 'password'}
+          label="Password"
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label={
+                  showPassword ? 'hide the password' : 'display the password'
+                }
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                onMouseUp={handleMouseUpPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+    </Box>
+  );
 }
