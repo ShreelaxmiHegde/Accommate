d31eda5a1366127e4ab119907bce031a31063c8b
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-12-03 10:05:17 +0530

    connect login logic to frontend

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,27 +1,16 @@
 const express = require("express");
 const wrapAsync = require("../utils/wrapAsync");
 const router = express.Router();
-const passport = require("passport");
-const { saveRedirectUrl } = require("../middleware.js");
+const { saveRedirectUrl, authenticate } = require("../middleware.js");
 const listingsController = require("../controllers/users.js");
 
-router.get("/me", (req, res) => {
-    res.send("@userme");
-});
-
 router.route("/signup")
-.get(listingsController.renderSignupFrom)
 .post(wrapAsync(listingsController.signup));
 
 router.route("/login")
-.get(listingsController.renderLoginFrom)
 .post( 
     saveRedirectUrl,
-    passport.authenticate("local", { 
-        failureRedirect: "/login", 
-        failureFlash: true 
-    }), 
-    listingsController.login
+    authenticate
 );
 
 router.get("/logout", listingsController.logout);
