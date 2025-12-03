63073eb61b622a56aa7a71f96b2a05bbe32fc667
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-12-03 18:46:22 +0530

    flash message component

---
+++ client/src/components/FlashMsg.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import {
+  Snackbar,
+  Alert
+} from '@mui/material';
+
+export default function FlashMsg({ flash, setFlash }) {
+  const [open, setOpen] = useState(false);
+
+  const handleClose = (evt, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    if (flash) {
+      setOpen(true);
+      setTimeout(() => {
+        setOpen(false)
+      }, 2000)
+    }
+  }, [flash]);
+
+  return (
+    <div>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert
+          onClose={handleClose}
+          severity={flash?.type}
+          variant="filled"
+          sx={{ width: '100%' }}
+        >
+          {flash?.msg} 
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+}
