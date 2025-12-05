e31fc8f61286df84ba3ca2174e27d71482306dc0
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-12-05 12:20:37 +0530

    rewrite listing schema & model

--- server/schema.js
+++ server/schema.js
@@ -1,16 +1,17 @@
 // schema validation
-
 const Joi = require("joi");
 
 module.exports.listingSchema = Joi.object({
     listing : Joi.object({
-        title : Joi.string().required().min(5).max(30),
-        desc : Joi.string().required().min(5).max(100),
-        location : Joi.string().required(),
-        state : Joi.string().required(),
+        title : Joi.string().required().min(5).max(100),
+        desc : Joi.string().required().min(5).max(1000),
+        address : Joi.string().required(),
+        stateCity : Joi.string().required(),
+        nearestCampus : Joi.string().required(),
+        propertyType : Joi.string().required(),
+        facilities : Joi.array().items(Joi.string()).required(),
         price : Joi.number().required().min(0),
-        capacity : Joi.number().required().min(1).max(20),
-        // image : Joi.string().allow("", null)
+        capacity : Joi.number().required().min(1)
     }).required()
 });
 
