79cc5d894992c7f0807e4b647b0209a7cca775dd
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-12-09 16:30:18 +0530

    add validate dropdown and checkbox enums

--- server/schema.js
+++ server/schema.js
@@ -1,16 +1,25 @@
 const Joi = require("joi");
 
+const facilityOptions = ["Wi-Fi", "Furnished", "Kitchen", "Laundry", "24/7 Water", "AC", "Private Bathroom"]
+const propertyTypes = ["pg", "hostel", "studio", "apartment", "house"]
+
 module.exports.listingSchema = Joi.object({
-    listing : Joi.object({
-        title : Joi.string().required().min(5).max(50),
-        desc : Joi.string().required().min(5).max(500),
-        address : Joi.string().required().min(3).max(200),
-        stateCity : Joi.string().required().min(3).max(50),
-        nearestCampus : Joi.string().required().min(3).max(50),
-        propertyType : Joi.string().required(),
-        facilities : Joi.array().items(Joi.string()).required(),
-        price : Joi.number().required().min(0),
-        capacity : Joi.number().required().min(1).max(50)
+    listing: Joi.object({
+        title: Joi.string().required().min(5).max(50),
+        desc: Joi.string().required().min(5).max(500),
+        address: Joi.string().required().min(3).max(200),
+        stateCity: Joi.string().required().min(3).max(50),
+        nearestCampus: Joi.string().required().min(3).max(50),
+        propertyType: Joi.string()
+            .valid(...propertyTypes)
+            .required(),
+        facilities: Joi.array()
+            .items(Joi.string()
+                .valid(...facilityOptions))
+                .min(1)
+            .required(),
+        price: Joi.number().required().min(0),
+        capacity: Joi.number().required().min(1).max(50)
     }).required()
 });
 
