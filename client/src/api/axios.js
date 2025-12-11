0d1139e2b9864fd1f4d07d5195a0807c920fbcc5
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-12-11 17:23:05 +0530

    catch errors from backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,21 @@
 //enable cookies in axios
+//all reqs made by this axios instanse go to backend
 import axios from "axios"
 
-export default axios.create({
+const api = axios.create({
     baseURL: "http://localhost:8080", //by default all reqs should go express-server port
     withCredentials: true //allow cookies to be sent/recived
 });
 
-//all reqs made by this axios instanse go to backend
+export function setInterceptors(setErr) {
+    api.interceptors.response.use(
+        (res) => res,
+        (err) => {
+            const msg = err.response?.data?.message || err.message || "Unexpected error!";
+            setErr(msg);
+            return Promise.reject(err);
+        }
+    )
+}
+
+export default api;
