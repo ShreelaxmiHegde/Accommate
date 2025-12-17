2d2367d01ab84fc65091200e0558b73a988a9cff
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-12-17 12:17:39 +0530

    add edit and delete review api

--- client/src/api/review.js
+++ client/src/api/review.js
@@ -1,7 +1,16 @@
 import api from "./axios"
 
-
 export const addReview = async(id, reviewData) => {
     let res = await api.post(`/listings/${id}/reviews`, reviewData);
+    return res.data;
+}
+
+export const editReview = async(id, reviewId, reviewData) => {
+    let res = await api.put(`/listings/${id}/reviews/${reviewId}/edit`, reviewData);
+    return res.data;
+}
+
+export const deleteReview = async(id, reviewId) => {
+    let res = await api.delete(`/listings/${id}/reviews/${reviewId}`);
     return res.data;
 }
