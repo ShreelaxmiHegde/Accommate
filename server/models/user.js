87b5014f176ec7731546728855c4733de5fd677d
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-12-18 09:40:49 +0530

    add username requirement in userschema to signin

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,11 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose"); // adds username and implements salting and hashing values automatically
 
 const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
     email : {
         type: String,
         required: true,
