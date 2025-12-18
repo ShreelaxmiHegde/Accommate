26820aab0e9db63ab7a10ca6df8e56736c87d707
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-12-18 11:59:11 +0530

    add cors frontend url

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const User = require("./models/user.js");
 const cors = require("cors");
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://accommate.vercel.app",
     credentials: true
 }));
 
